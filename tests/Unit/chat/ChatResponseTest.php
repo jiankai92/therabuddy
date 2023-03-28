986db8c784e79db64cc0c8a0dfbe4e98640a2a61
Jian Kai <jiankai@openmindsresources.com>
2023-03-29 01:58:37 +0800

    Updated Example test to return assert redirect to chat page. Added Chat response test to assert string for the output format of ChatService submitMessageAndGetResponse function

---
+++ tests/Unit/chat/ChatResponseTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\chat;
+
+use App\Services\Chat\ChatService;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ChatResponseTest extends TestCase
+{
+    use withFaker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * A basic unit test example.
+     */
+    public function testChatResponseFormat(): void
+    {
+        $chat_service = app()->make(ChatService::class);
+        $chat_message = $this->faker->sentence;
+        $this->assertIsString($chat_service->submitMessageAndGetResponse($chat_message));
+    }
+}
