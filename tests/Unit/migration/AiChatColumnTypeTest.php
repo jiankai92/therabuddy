0ec54b619c0098ee84a81ed2447ae7b5a1914924
Jian Kai <jiankai@openmindsresources.com>
2023-05-04 23:55:59 +0800

    Refactor CreateAiChatTableTest.php to AiChatColumnTypeTest.php and removed corresponding column and table created test (moved to CreateDbTablesTest.php)

--- tests/Unit/migration/AiChatColumnTypeTest.php
+++ tests/Unit/migration/AiChatColumnTypeTest.php
@@ -8,19 +8,19 @@ use Doctrine\DBAL\Types\Types as DBALTypes;
 use Tests\TestCase;
 
 //Import functions for testing
-use App\Models\AiChatModel;
+use App\Models\AiChat;
 
-class CreateAiChatTableTest extends TestCase
+class AiChatColumnTypeTest extends TestCase
 {
     use DatabaseMigrations;
 
-    private AiChatModel $chat_model;
+    private AiChat $chat_model;
     private array $db_columns;
 
     public function setUp(): void
     {
         parent::setUp();
-        $this->chat_model = new AiChatModel();
+        $this->chat_model = new AiChat();
 
         // Reference columns based on fillable and hidden properties defined in model
         $this->db_columns = array_merge(
@@ -28,38 +28,16 @@ class CreateAiChatTableTest extends TestCase
             $this->chat_model->getFillable(),
             $this->chat_model->getHidden()
         );
-
-    }
-
-    /**
-     * Check if ai_chats table is created
-     * @group database-setup
-     */
-    public function test_ai_chat_table_created(): void
-    {
-        $this->assertTrue(Schema::hasTable($this->chat_model->getTable()));
-    }
-
-    /**
-     * Check if ai_chats table is created with the required columns names
-     * @group database-setup
-     */
-    public function test_ai_chat_columns_created(): void
-    {
-        $this->assertEquals(
-            Schema::getColumnListing($this->chat_model->getTable()),
-            $this->db_columns
-        );
     }
 
     /**
      * Check if ai_chats table is created with the required column types
      * @group database-setup
      */
-    public function test_ai_chat_columns_type(): void
+    public function test_created_ai_chat_columns_type(): void
     {
         $table_name = $this->chat_model->getTable();
-        foreach (Schema::getColumnListing($this->chat_model->getTable()) as $col) {
+        foreach (Schema::getColumnListing($table_name) as $col) {
             switch ($col) {
                 case 'id':
                     $this->assertSame(DBALTypes::BIGINT, Schema::getColumnType($table_name, $col));
@@ -70,11 +48,7 @@ class CreateAiChatTableTest extends TestCase
                 case 'session_id':
                     $this->assertSame(DBALTypes::STRING, Schema::getColumnType($table_name, $col));
                     break;
-                case 'messages':
-                    $this->assertSame(DBALTypes::JSON, Schema::getColumnType($table_name, $col));
-                    break;
                 case 'created_at':
-                case 'updated_at':
                     $this->assertSame(DBALTypes::DATETIME_MUTABLE, Schema::getColumnType($table_name, $col));
                     break;
                 default:
