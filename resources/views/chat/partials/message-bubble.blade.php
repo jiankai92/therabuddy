6b8b7517b4914bbb42d2d833b38861c5bab0e1f0
Jian Kai <jiankai@openmindsresources.com>
2023-05-29 23:45:29 +0800

    Updated APP_NAME values and set SESSION_DRIVER to redis in .env.example file
    Updated SESSION_LIFETIME to 1440 (1 day / 1440 minutes)
    
    prompt Chat bubble fix to align text to right

--- resources/views/chat/partials/message-bubble.blade.php
+++ resources/views/chat/partials/message-bubble.blade.php
@@ -3,7 +3,7 @@
         <span class="text-xs">Me</span>
         <span class="text-xs">{{$time}}</span> &nbsp; &nbsp;
     </div>
-    <div class="text-base text-white rounded bg-sky-300 mb-7 p-5 leading-6 w-11/12 float-right">
+    <div class="text-base test-left text-white rounded bg-sky-300 mb-7 p-5 leading-6 w-11/12 float-right">
         {{ $message }}
     </div>
 </li>
